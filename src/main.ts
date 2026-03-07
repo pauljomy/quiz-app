716816b13b34a10571c7b374d4b60f242ea727ef
Paul Jomy <pauljomy@users.noreply.github.com>
2026-03-07 17:55:24 +0530

    working on ts file

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,26 @@
+import { quizzes } from "./data.json";
+
 const toggle = document.querySelector(
   "input[type='checkbox']",
 ) as HTMLInputElement;
+const headingEl = document.getElementById("heading");
+const questionsEl = document.getElementById("questions");
+
+const logoEL = document.getElementById("logo");
+
+document.addEventListener("click", (e) => {
+  const target = e.target as HTMLElement;
+
+  if (target.dataset.subject === "html") {
+    htmlQuestions();
+  }
+});
+
+function htmlQuestions() {
+  quizzes.forEach((quiz) => {
+    console.log(quiz.title);
+  });
+}
 
 if (localStorage.theme === "dark") {
   document.documentElement.classList.add("dark");
